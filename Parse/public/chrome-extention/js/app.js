04d09e8a7d55eda80fa38fb8bc4118ac65ec7a27
Luke Davis <lukefable@gmail.com>
2015-02-19 07:36:11 +0000

    built the beginnings of a chrome extention

---
+++ Parse/public/chrome-extention/js/app.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+	app.c.init();
+});
+/////////////////////////////
+
+var app={m:{},v:{},c:{},t:{}};
+
+/////////////////////////////
+
+
+/////////////////////////////
+
+app.c.init=function(){
+	app.v.init();
+	app.v.listeners();
+};
+
+//////////////////////////////
+
+app.v.init=function(){
+	$("body").html(app.t.splash() );
+};
+
+
+app.v.listeners=function(){
+
+};
+
+//////////////////////////////
+
+app.t.splash=function(){
+  var d="";
+  d+="<img src='icon.png' alt='resume ai icon' />";
+  d+="<div class='wrapper'>";
+    d+="<input type='text' placeholder='Organization'></input>";
+    
+    d+="<input type='text' placeholder='Role'></input>";
+    d+="<textarea rows='10' placeholder='Bullshit'></textarea>";  
+  d+="<input type='button' value='Save'></input>";
+  d+="</div>";    
+  return d;
+};
