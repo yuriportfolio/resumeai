41194e07ad92a3d02dd10064878b0cb7421c9879
Luke Davis <lukefable@gmail.com>
2015-04-05 04:12:25 +0000

    updated chrome extension with  new field for cover letter blurb.

--- Parse/public/chrome-extention/js/app.js
+++ Parse/public/chrome-extention/js/app.js
@@ -11,7 +11,8 @@ var app={m:{},v:{},c:{},t:{}};
 /////////////////////////////
 
 app.c.init=function(){
-	app.v.init();
+  Parse.initialize(creds().apk, creds().jsk);
+  app.v.init();
 	app.v.listeners();
 };
 
@@ -23,7 +24,24 @@ app.v.init=function(){
 
 
 app.v.listeners=function(){
-
+  $("body").on("click","#save-to-parse",function(){
+    var organization = _.escape($("#organization").val() );
+    var role = _.escape($("#role").val() );
+    var description = _.escape($("#description").val() );
+    var description = _.escape($("#coverLetter").val() );
+    $("#organization").val("");
+    $("#role").val("");
+    $("#description").val("");
+    $("#coverLetter").val("");
+    
+    var Lead = Parse.Object.extend("Leads", {});
+    var lead = new Lead();
+    lead.set("organization",organization);
+    lead.set("role",role);
+    lead.set("description",description);
+    lead.set("coverLetter",coverLetter);
+    lead.save();
+  });
 };
 
 //////////////////////////////
@@ -32,11 +50,11 @@ app.t.splash=function(){
   var d="";
   d+="<img src='icon.png' alt='resume ai icon' />";
   d+="<div class='wrapper'>";
-    d+="<input type='text' placeholder='Organization'></input>";
-    
-    d+="<input type='text' placeholder='Role'></input>";
-    d+="<textarea rows='22' placeholder='Bullshit'></textarea>";  
-  d+="<input type='button' value='Save'></input>";
+    d+="<input type='text' placeholder='Organization' id='organization'></input>";
+    d+="<input type='text' placeholder='Role' id='role'></input>";
+    d+="<textarea rows='16' placeholder='Cover Letter Text' id='coverLetter'></textarea>";  
+    d+="<textarea rows='16' placeholder='Lead' id='description'></textarea>";  
+  d+="<input type='button' value='Save' id='save-to-parse'></input>";
   d+="</div>";    
   return d;
 };
